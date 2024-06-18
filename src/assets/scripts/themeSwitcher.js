ac487140cca47c9ba803ea0be7901e5f40cce2a2
panakir <panakir.webdev@gmail.com>
2024-06-18 11:46:34 +0300

    feat: add build (Tue, Jun 18, 2024 11:46AM)

--- src/assets/scripts/themeSwitcher.js
+++
@@ -1,7 +0,0 @@
-const switcher = document.getElementById("switcher");
-
-const toggleTheme = () => {
-  document.body.classList.toggle("dark-theme");
-};
-
-switcher.addEventListener("click", toggleTheme);
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import "../index.html";
-import "./main.scss";
-import "./assets/scripts/burgerMenu";
-import "./assets/scripts/themeSwitcher";
